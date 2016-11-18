23c4d9dc755f295e91ca9bac6130552946b59146
whatPanache <pwcarney@bu.edu>
2016-11-18 08:17:37 -0500

    Done

--- goldilocks.py
+++ goldilocks.py
@@ -2,14 +2,20 @@
 def choose_chairs(input):
     
     with open(input) as file:
-        input_text = file.readlines()
+        input_text = file.read().splitlines() 
         
-    goldi_weight, goldi_temp = input_text[0].split(' ')
+    goldi_weight, goldi_temp = map(int, input_text[0].split(' '))
     input_text.pop(0)
     
-    for input in input_text:
-        pass
-    
+    good_chairs = []
+    for ind, input in enumerate(input_text):
+        input_weight, input_temp = map(int, input.split(' '))
+        
+        if (goldi_weight < input_weight) and (goldi_temp > input_temp):
+            good_chairs.append(ind+1)
 
+    print(good_chairs)
+        
+        
 if __name__ == "__main__":
-    choose_chairs('input.txt')
+    choose_chairs('challenge_input.txt')
